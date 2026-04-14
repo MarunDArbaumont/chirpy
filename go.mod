d22ee19a2fcbfb42eb0173d183934d2f89f7e507
Marun <marin.maulbon@gmail.com>
2026-04-14 13:26:25 +0200

    first step in making a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MarunDArbaumont/chirpy
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"log"
+	"net/http"
+)
+
+func main () {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	serv := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(serv.ListenAndServe())
+}
